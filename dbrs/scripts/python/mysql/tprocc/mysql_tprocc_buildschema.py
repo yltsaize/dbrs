204ac6dea119f019ad7412bf12d2b858b98bbee5
Rophy Tsai <rophy@users.noreply.github.com>
2023-02-11 13:12:00 +0800

    feat: adjust config for hammerdb/mysql scripts

--- dbrs/scripts/python/mysql/tprocc/mysql_tprocc_buildschema.py
+++ dbrs/scripts/python/mysql/tprocc/mysql_tprocc_buildschema.py
@@ -5,22 +5,17 @@ print("SETTING CONFIGURATION")
 dbset('db','mysql')
 dbset('bm','TPC-C')
 
-diset('connection','mysql_host','localhost')
+diset('connection','mysql_host','dbrs-mysql-src')
 diset('connection','mysql_port','3306')
-diset('connection','mysql_socket','/tmp/mysql.sock')
+# diset('connection','mysql_socket','/tmp/mysql.sock')
 
-vu = tclpy.eval('numberOfCPUs')
-warehouse = int(vu) * 5
-diset('tpcc','mysql_count_ware',warehouse)
-diset('tpcc','mysql_num_vu',vu)
+diset('tpcc','mysql_count_ware',1)
+diset('tpcc','mysql_num_vu',1)
 diset('tpcc','mysql_user','root')
-diset('tpcc','mysql_pass','mysql')
+diset('tpcc','mysql_pass','passw0rd')
 diset('tpcc','mysql_dbase','tpcc')
 diset('tpcc','mysql_storage_engine','innodb')
-if (warehouse >= 200): 
-    diset('tpcc','mysql_partition','true') 
-else:
-    diset('tpcc','mysql_partition','false') 
+diset('tpcc','mysql_partition','false')
 
 print("SCHEMA BUILD STARTED")
 buildschema()
